fbdaf01efec7527247c04cf8b0927793591c61eb
Darshan Parbadiya <darshanparbadiya@gmail.com>
2024-12-17 14:12:11 -0500

    login-forms-added

--- apps/docs/app/page.tsx
+++ apps/docs/app/page.tsx
@@ -1,9 +1,16 @@
+import { LoginForm } from "@repo/ui/components/login-form";
 import { Button } from "@repo/ui/components/ui/button";
 
 export default function Page() {
   return (
     <main>
-      <Button>Click me</Button>
+      <h1>Form!</h1>
+      {/* <Button>Click me</Button> */}
+      <div className="flex min-h-svh w-full items-center justify-center p-6 md:p-10">
+      <div className="w-full max-w-sm">
+        <LoginForm />
+      </div>
+    </div>
     </main>
   );
 }
---
+++ notes/deployment.md
@@ -0,0 +1,8 @@
+turbo run build --filter=web
+
+login
+pnpm dlx turbo login
+
+npx turbo link
+
+rm -rf .turbo/cache
